f8a1112b01e257db36877b91e9403f6c1b940288
Ramy Khorshed <ramykhorshed@gmail.com>
2017-09-15 02:21:56 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,9 +5,15 @@ def second_challenge
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-
+  puts "a7a"
   #code your solution here!
-
-  
-
+  array =[]
+  groceries.each do |type, value|
+    value.each do |food|
+      array << "#{food}"
+    end
+  end
+  array
 end
+
+second_challenge
